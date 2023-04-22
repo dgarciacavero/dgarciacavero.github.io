524764db876a25dc4b7aea4685a4f511ab319b97
dgarciacavero <131523609+dgarciacavero@users.noreply.github.com>
2023-04-22 19:33:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function mostrarSublista() {
+  var sublista = document.getElementById("sub-menu1");
+  if (sublista.style.display === "none") {
+    sublista.style.display = "block";
+  } else {
+    sublista.style.display = "none";
+  }
+}
+
